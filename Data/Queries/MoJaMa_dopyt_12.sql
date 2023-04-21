3a2bccb27c1986ff0f08f647d528c91bd71e0f4f
JHJohny <ondis.jan1@gmail.com>
2023-04-21 17:13:18 +0200

    New query added

---
+++ Data/Queries/MoJaMa_dopyt_12.sql
@@ -0,0 +1,24 @@
+/*
+ * Geting document, based on archive id
+ */
+SELECT 
+	document.doc_name AS 'Document name',
+	archive.comment AS 'Document comment',
+	CONCAT(employee.first_name, " ", employee.last_name) AS 'Referent',
+	CONCAT(location.description, ", ", section.description) AS 'Document location',
+	archive.archivation_date AS 'Archivation date',
+	access_level.access_requirements AS 'Access requirements',
+	CASE 
+		WHEN archive_code.archive_code_expiration < DATE(NOW()) THEN 'No' 
+		ELSE 'Yes' 
+	END AS 'Is code valid'
+FROM 
+	archive
+INNER JOIN document ON archive.doc_id = document.doc_id
+INNER JOIN employee ON archive.referent_id = employee.emp_id
+INNER JOIN section ON archive.section_id = section.section_id
+INNER JOIN location ON section.location_id = location.location_id
+INNER JOIN access_level ON location.security_level_access = access_level.access_level 
+INNER JOIN archive_code ON archive.arch_code = archive_code.archive_code 
+WHERE archive.arch_id = 50
+LIMIT 1;
