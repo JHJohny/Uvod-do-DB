38efe66bb21ce56ec11e2a5942382c197a94d923
JHJohny <ondis.jan1@gmail.com>
2023-04-21 16:44:31 +0200

    First query refactored

--- Data/Queries/MoJaMa_dopyt_01.sql
+++ Data/Queries/MoJaMa_dopyt_01.sql
@@ -6,12 +6,19 @@ SELECT
 	archive.comment AS 'Document comment',
 	CONCAT(employee.first_name, " ", employee.last_name) AS 'Referent',
 	CONCAT(location.description, ", ", section.description) AS 'Document location',
-	archive.archivation_date AS 'Archivation date'
-FROM 
+	archive.archivation_date AS 'Archivation date',
+	access_level.access_requirements AS 'Access requirements',
+	CASE
+		WHEN archive_code.archive_code_expiration < DATE(NOW()) THEN 'No'
+		ELSE 'Yes'
+	END AS 'Is code valid'
+FROM
 	archive
 INNER JOIN document ON archive.doc_id = document.doc_id
 INNER JOIN employee ON archive.referent_id = employee.emp_id
 INNER JOIN section ON archive.section_id = section.section_id
 INNER JOIN location ON section.location_id = location.location_id
+INNER JOIN access_level ON location.security_level_access = access_level.access_level
+INNER JOIN archive_code ON archive.arch_code = archive_code.archive_code
 ORDER BY archivation_date DESC
 LIMIT 20;
